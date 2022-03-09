677d16fd9e717e30291f4384178f4a377e61f3b9
Andrew LeRoux <andrewleroux2639@gmail.com>
2022-03-09 11:27:02 -0800

    commit tree dca3b3468a7aea57591bb35de51d1ca687df99fa

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,24 @@
 # Your code goes here!
+class Anagram
+attr_reader :keyword
+    def initialize(keyword)
+        @keyword = keyword
+    end
+
+    def match(arr)
+        answer_arr = []
+        arr.each do |word|
+            word_arr = word.chars.sort
+            keyword_sorted = @keyword.chars.sort
+            if word_arr == keyword_sorted
+                answer_arr.push(word)
+            end
+        end
+        answer_arr
+    end
+
+end
+
+
+listen = Anagram.new("listen")
+listen.match(%w[enlists google inlets banana])
